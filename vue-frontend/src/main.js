40e7326564c19c23769c14817642d41d70019038
Pedro Lucas de Moliner de Castro <pedro.lucas.moliner.castro@gmail.com>
2022-06-16 00:40:53 -0300

    Main Format

--- vue-frontend/src/main.js
+++ vue-frontend/src/main.js
@@ -1,46 +1,48 @@
-import { createApp } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
-import App from './App.vue'
+import { createApp } from "vue";
+import { createRouter, createWebHistory } from "vue-router";
+
+
+// Font Awesome
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faCircleUser, faCartShopping, faBars, faMagnifyingGlass, faCaretDown, faMinus, faPlus } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faCircleUser, faCartShopping, faBars, faMagnifyingGlass, faCaretDown, faMinus, faPlus);
+
+
+// Maska
+import Maska from "maska";
+
+
+import App from "@/App.vue";
+import Home from "@/pages/Home.vue";
+import Cart from "@/pages/Cart.vue";
+import Login from "@/pages/Login.vue";
+import Admin from "@/pages/Admin.vue";
+import Product from "@/pages/Product.vue";
+import Profile from "@/pages/Profile.vue";
+import Products from "@/pages/Products.vue";
 
 
 // Router Setup
-import Home from './pages/Home.vue'
-import Login from './pages/Login.vue'
-import Products from './pages/Products.vue'
-import Product from './pages/Product.vue'
-import Checkout from './pages/Checkout.vue'
-import Cart from './pages/Cart.vue'
-import Profile from './pages/Profile.vue'
-import Admin from './pages/Admin.vue'
-
-// Mapping components to routes
 const routes = [
-  { path: '/', component: Home },
-  { path: '/login', component: Login },
-  { path: '/products', component: Products },
-  { path: '/products/:id', component: Product },
-  { path: '/cart', component: Cart },
-  { path: '/checkout', component: Checkout },
-  { path: '/profile', component: Profile },
-  { path: '/admin', component: Admin },
-]
+    { path: '/', component: Home },
+    { path: "/cart", component: Cart },
+    { path: "/login", component: Login },
+    { path: "/admin", component: Admin },
+    { path: "/profile", component: Profile },
+    { path: "/products", component: Products },
+    { path: "/products/:id", component: Product },
+];
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes,
-})
+    history: createWebHistory(),
+    routes,
+});
 
-// Font Awesome Setup
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faCircleUser, faCartShopping, faBars, faMagnifyingGlass, faCaretDown, faMinus, faPlus } from '@fortawesome/free-solid-svg-icons'
-// import faRegular from '@fortawesome/free-regular-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import Maska from 'maska'
-
-library.add(faCircleUser, faCartShopping, faBars, faMagnifyingGlass, faCaretDown, faMinus, faPlus)
 
 createApp(App)
-  .use(router)
-  .use(Maska)
-  .component("font-awesome-icon", FontAwesomeIcon)
-  .mount("#app")
+    .use(router)
+    .use(Maska)
+    .component("font-awesome-icon", FontAwesomeIcon)
+    .mount("#app");
